20271b0136cdcc045ccd62faca54913a8b8f8599
Joao Mateus <jaomateus@gmail.com>
2022-11-15 15:06:58 +0100

    refactored

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # index
+  get 'tasks', to: 'tasks#index'
+
+  # CREATE
+  # GET the task creation form = 1 request
+  get 'tasks/new', 'tasks#new'
+  # display one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # POST the parameters to create a new restaurant = 1 request
+  post 'tasks', to: 'tasks#create'
+
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
